daad466ca763636d9f8fae9c52613eb0bd1d6b15
Dmitry <mr.blacky01@gmail.com>
2016-07-29 18:48:48 +0300

    add class for View web content

---
+++ app/src/main/java/com/example/mrblacky/bsuirnews/WebResourse.java
@@ -0,0 +1,56 @@
+package com.example.mrblacky.bsuirnews;
+
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.TextView;
+
+public class WebResourse extends AppCompatActivity {
+
+    private WebView browser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_resourse);
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String product = extras.getString("HrefValue");
+            //TextView tv = (TextView) findViewById(R.id.href);
+            //tv.setText(product  );
+            browser=(WebView)findViewById(R.id.webBrowser);
+            browser.setWebViewClient(new WebViewClientForDoPage());
+
+            browser.getSettings().setBuiltInZoomControls(true);
+            browser.getSettings().setSupportZoom(true);
+            browser.getSettings().setUseWideViewPort(true);
+            //browser.getSettings().setLoadWithOverviewMode(false);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                browser.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.TEXT_AUTOSIZING);
+            } else {
+                browser.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NORMAL);
+            }
+
+            try
+            {
+                browser.loadUrl(product);
+            }
+            catch (Exception exept)
+            {
+
+            }
+
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(browser.canGoBack()) {
+            browser.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
