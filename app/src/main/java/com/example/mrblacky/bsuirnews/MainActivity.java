6e8007d36b1ab7ba038c51ab06388e68e8a78212
Dmitry <mr.blacky01@gmail.com>
2016-08-03 01:03:21 +0300

    make vertical screen orientation

--- app/src/main/java/com/example/mrblacky/bsuirnews/MainActivity.java
+++ app/src/main/java/com/example/mrblacky/bsuirnews/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.mrblacky.bsuirnews;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 }
