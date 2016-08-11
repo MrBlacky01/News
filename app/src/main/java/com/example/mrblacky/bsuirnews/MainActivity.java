8e79b928816866147645799120f6ecb658df7816
Dmitry <mr.blacky01@gmail.com>
2016-08-11 22:02:50 +0300

    work with navigation drawer

--- app/src/main/java/com/example/mrblacky/bsuirnews/MainActivity.java
+++ app/src/main/java/com/example/mrblacky/bsuirnews/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.mrblacky.bsuirnews;
 
 import android.content.pm.ActivityInfo;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String[] NavigationDrawerList;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +22,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        NavigationDrawerList = getResources().getStringArray(R.array.navigation_drawer_array);
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,R.layout.drawer_list_item,NavigationDrawerList));
+
+
     }
 
     @Override
